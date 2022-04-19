0d8b3cc19a750353ea3bfb045e7318184fcfa233
cmeggs0 <cmeggs0@chicagobooth.edu>
2022-04-19 20:22:26 +0000

    locked down controller actions with before actions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,14 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
 
+  before_action :ensure_owner_is_current_user, only: %i[ edit update destroy ]
+
+  def ensure_owner_is_current_user
+    if @photo.owner != current_user
+      redirect_back fallback_location: root_path, alert: "You don't have access to this"
+    end
+  end
+
   # GET /photos or /photos.json
   def index
     @photos = Photo.all
